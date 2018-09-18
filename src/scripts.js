94614ab3decd6559fe80ca77308774cd9e27556d
Nick <nick.tchemi@gmail.com>
2018-09-17 22:11:27 -0700

    refactored code. Added CSS

--- src/scripts.js
+++ src/scripts.js
@@ -1,15 +1,20 @@
+// target input to load file
 function el(id) {
     return document.getElementById(id);
 }
 
-var canvas = el("colors_sketch");
-var context = canvas.getContext("2d");
+el("fileUpload").addEventListener("change", readImage, false);
 
+// const to define canvas properties
+const canvas = el("colors_sketch");
+const context = canvas.getContext("2d");
+
+// process uploaded image file to canvas
 function readImage() {
     if (this.files && this.files[0]) {
         var FR = new FileReader();
         FR.onload = function (e) {
-            var img = new Image();
+            const img = new Image();
             img.onload = function () {
                 context.drawImage(img, 0, 0, img.width, img.height, 0, 0, 300, 200);
                 findAvgColor();
@@ -17,19 +22,21 @@ function readImage() {
             img.src = e.target.result;
         };
         FR.readAsDataURL(this.files[0]);
+        document
+          .getElementById("result")
+          .classList.remove("default-hide");
     }
 }
 
-el("fileUpload").addEventListener("change", readImage, false);
 
-// helpers
-var getAverageRGB = function (imgData) {
-    var red = 0;
-    var green = 0;
-    var blue = 0;
-    var total = 0;
+// helper function to obtain average rgb
+const getAverageRGB = function (imgData) {
+    let red = 0;
+    let green = 0;
+    let blue = 0;
+    let total = 0;
 
-    for (var i = 0; i < imgData.length; i += 4) {
+    for (let i = 0; i < imgData.length; i += 4) {
         if (imgData[i + 3] !== 0) {
             red += imgData[i + 0];
             green += imgData[i + 1];
@@ -38,17 +45,17 @@ var getAverageRGB = function (imgData) {
         }
     }
 
-    var avgRed = Math.floor(red / total);
-    var avgGreen = Math.floor(green / total);
-    var avgBlue = Math.floor(blue / total);
+    let avgRed = Math.floor(red / total);
+    let avgGreen = Math.floor(green / total);
+    let avgBlue = Math.floor(blue / total);
 
     return "rgba(" + avgRed + "," + avgGreen + "," + avgBlue + ", 1)";
 };
 
 // process uploaded image to extract average rgba
 function findAvgColor() {
-    var pixels = context.getImageData(10, 10, 280, 180);
-    var averageRGBA = getAverageRGB(pixels.data);
+    const pixels = context.getImageData(10, 10, 280, 180);
+    const averageRGBA = getAverageRGB(pixels.data);
     document.getElementById("color_palette").style.backgroundColor = averageRGBA;
     console.log(averageRGBA);
 }
